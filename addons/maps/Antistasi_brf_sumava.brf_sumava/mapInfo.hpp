b9f969e6b692550b50125611c6eb1912046645db
aderq <aderq.xxx@gmail.com>
2023-04-10 19:11:14 +0200

    Correction to RT coords, garrison marker for test

--- addons/maps/Antistasi_brf_sumava.brf_sumava/mapInfo.hpp
+++ addons/maps/Antistasi_brf_sumava.brf_sumava/mapInfo.hpp
@@ -2,10 +2,12 @@ class brf_sumava {
 	population[] = {}; //automated system is good enough, no need for manual population
 	disabledTowns[] = {};
 	antennas[] = {
-		{5352.76,7838.3,0.368622},{9012.25,2230.76,0},{11195.5,7491.4,1.20309},{768.341,3056.67,0.000862122},{310.292,2006.35,0.234848},{713.314,11490.4,0.22364}
+		{5347.78,7835.67,0.393112},{9012.25,2230.76,0},{11195.5,7491.4,1.20309},{768.341,3056.67,0.000862122},{310.292,2006.35,0.234848},{713.314,11490.4,0.22364}
 	};
 	banks[] = {}; // no banks on map
-	garrison[] = {}; // not sure how to set this up
+	garrison[] = {
+		{},{"airport_2"},{},{}
+	}; // not sure how to set this up
 	fuelStationTypes[] = {
 		{"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
 	};
