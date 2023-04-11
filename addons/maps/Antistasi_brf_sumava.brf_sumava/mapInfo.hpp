4f0fcc5b4c95efdcfd39b691f96395dc562578ce
AderQ <aderq.xxx@gmail.com>
2023-04-11 10:28:18 +0200

    removed comments

--- addons/maps/Antistasi_brf_sumava.brf_sumava/mapInfo.hpp
+++ addons/maps/Antistasi_brf_sumava.brf_sumava/mapInfo.hpp
@@ -1,13 +1,13 @@
 class brf_sumava {
 	population[] = {}; //automated system is good enough, no need for manual population
-	disabledTowns[] = {};
+	disabledTowns[] = {}; //no need to disable towns
 	antennas[] = {
 		{5347.78,7835.67,0.393112},{9012.25,2230.76,0},{11195.5,7491.4,1.20309},{768.341,3056.67,0.000862122},{310.292,2006.35,0.234848},{713.314,11490.4,0.22364}
 	};
 	banks[] = {}; // no banks on map
 	garrison[] = {
 		{},{"airport_2", "control_5"},{},{"airport_2"}
-	}; // not sure how to set this up
+	};
 	fuelStationTypes[] = {
 		{"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
 	};
